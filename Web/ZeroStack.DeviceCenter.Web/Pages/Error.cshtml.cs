7af5cf782cc676745a9c5e7f09a2df9381a2105a
意 张 <z1494934626@outlook.com>
2023-02-07 15:12:26 +0800

    更新为6.0版本

--- Web/ZeroStack.DeviceCenter.Web/Pages/Error.cshtml.cs
+++ Web/ZeroStack.DeviceCenter.Web/Pages/Error.cshtml.cs
@@ -19,7 +19,7 @@ namespace ZeroStack.DeviceCenter.Web.Pages
             _logger = logger;
         }
 
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
